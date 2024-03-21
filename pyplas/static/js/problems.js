18f869a27279e71421e15a0f01b1a29c8eed9b2e
hary <p3pq5n6r@s.okayama-u.ac.jp>
2024-03-21 14:49:09 +0900

    build node module

--- pyplas/static/js/problems.js
+++ pyplas/static/js/problems.js
@@ -1,9 +1,11 @@
 // global variable:  execute task que
 execute_node_q = []
+// 
 
 $(function() {
     var ws = setUpKernel()
-
+    var exec_count = 0
+    
     $(".node-code").each(function(index) {
         var id = $(this).attr("id")
         var editor = ace.edit(id);
@@ -12,7 +14,7 @@ $(function() {
         if ($(this).attr("class").includes("readonly")) {
             editor.setReadOnly(true)
         }
-    
+        console.log(id)
         editor.getSession().on("change", function(delta) {
             var line = editor.session.getLength()
             if (line > 4) {
@@ -34,11 +36,17 @@ $(function() {
         kernelRestart(kernel_id, async=false)
         execute_node_q = []
         ws = setUpKernel(kernel_start=false)
+        exec_count = 0
+        $(".node-number").each(function(idx) {
+            $(this).text(" * ")
+        })
     })
     
     $(window).on("keydown", async function(e) {
         if (e.ctrlKey) {
             if (e.keyCode == 13) { // Ctrl-Enter
+                exec_count += 1
+                $current_node.find(".node-number").text(exec_count)
                 execute_node_q.push($current_node)
                 executeCode($current_node, ws)
                 return false
--- pyplas/uimodules.py
+++ pyplas/uimodules.py
@@ -1,5 +1,13 @@
 from tornado.web import UIModule 
+import uuid 
 
 class Node(UIModule):
-    def render(self, ):
-        pass
+    def render(self, code:str="", 
+               editor_id:str=None, readonly: bool=False) -> str:
+        if editor_id is None:
+            editor_id = str(uuid.uuid4())
+        readonly = "readonly" if readonly else ""
+        return self.render_string("modules/node.html", 
+                                  editor_id=str(editor_id),
+                                  code=code,
+                                  readonly=readonly) 
