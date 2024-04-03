aaa70ed52326cfc6743d2457704984871c6c5b6a
hary <p3pq5n6r@s.okayama-u.ac.jp>
2024-04-03 16:50:39 +0900

    implemented problem_page overview

---
+++ pyplas/html-design/plas.js
@@ -0,0 +1,16 @@
+
+
+document.querySelectorAll(".node-code").forEach(function(elem) {
+    var id = crypto.randomUUID()
+    elem.id = id
+    var editor = ace.edit(id, {
+        mode: "ace/mode/python",
+        theme: "ace/theme/twilight"
+    });
+    editor.setOptions({
+        autoScrollEditorIntoView: true,
+        maxLines: 100,
+        minLines: 5
+    });
+    console.log(id)
+})
