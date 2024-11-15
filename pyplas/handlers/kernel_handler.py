0e8f461ce09bc77ec322a9e82521bc710278784f
hary84 <p3pq5n6r@s.okayama-u.ac.jp>
2024-11-15 15:43:50 +0900

    add tests for kernel_handler.py

--- pyplas/handlers/kernel_handler.py
+++ pyplas/handlers/kernel_handler.py
@@ -1,4 +1,3 @@
-import asyncio
 from typing import Optional
 
 from jupyter_client.multikernelmanager import DuplicateKernelError
@@ -27,7 +26,7 @@ class KernelHandler(ApplicationHandler):
             kernel_ids = g.km.list_kernel_ids()
             self.finish({
                 "kernel_ids": kernel_ids,
-                "DESCR": "Get a list of kernels managed by the server."
+                "DESCR": "Got a list of currently running kernels"
             })
 
         # GET /kernel/<k_id>
@@ -39,7 +38,7 @@ class KernelHandler(ApplicationHandler):
             self.finish({
                 "kernel_id": k_id,
                 "is_alive": is_alive,
-                "DESCR": "Get the kernel state(alive or not)"
+                "DESCR": "Got the state of the kernel(alive or not)"
             })
 
         # GET /kernel/<k_id>/<action>
@@ -74,19 +73,21 @@ class KernelHandler(ApplicationHandler):
                     self.set_status(404, reason=f"{self.request.uri} is not Found.")
                     self.finish()
         except DuplicateKernelError as e:
-            self.set_status(500, reason=f"kernel({k_id}) is already exist.")
+            self.set_status(409, reason=f"kernel(id='{k_id}') is already exist.")
             self.finish()
         except KeyError as e:
-            self.set_status(500, reason=f"kernel_id({k_id}) is not found in KM.")
+            self.set_status(404, reason=f"kernel(id='{k_id}') is not found.")
             self.finish()
         except Exception as e:
             mylogger.error(e, exc_info=True)
-            self.set_status(500, reason="internal server error")
+            self.set_status(500, reason="Internal Server Error")
             self.finish()
 
     async def kernel_start(self, kernel_id:Optional[str]=None) -> None:
         """
         カーネルを起動する
+
+        重複した`kernel_id`が渡された場合, `DupulicateKernelError`を投げる
         """
         try:
             if kernel_id is None:
@@ -98,11 +99,13 @@ class KernelHandler(ApplicationHandler):
         else:
             self.finish({"kernel_id": self.kernel_id,
                          "DESCR": "Kernel is successfully started."})
-            mylogger.info(f"kernel(kernel_id={kernel_id}) is started.")
+            mylogger.info(f"Starting a kernel(id='{kernel_id}')")
 
     async def kernel_restart(self, kernel_id:str) -> None:
         """
         カーネルを再起動する
+
+        存在しない`kernel_id`が渡された場合, `KeyError`を投げる
         """
         try:
             await g.km.shutdown_kernel(kernel_id=kernel_id)
@@ -112,11 +115,13 @@ class KernelHandler(ApplicationHandler):
         else:
             self.finish({"kernel_id": kernel_id,
                          "DESCR": "Kernel is successfully restarted."})
-            mylogger.info(f"kernel(kernel_id={kernel_id}) is restarted.")
+            mylogger.info(f"Restarting a kernel(id='{kernel_id}')")
 
     async def kernel_interrupt(self, kernel_id:str) -> None:
         """
         カーネルの実行を中断する
+
+        存在しない`kernel_id`が渡された場合, `KeyError`を投げる
         """
         try:
             km = g.km.get_kernel(kernel_id)
@@ -126,7 +131,7 @@ class KernelHandler(ApplicationHandler):
         else:
             self.finish({"kernel_id": kernel_id,
                          "DESCR": "Kernel is successfully interrupted."})
-            mylogger.info(f"kernel(kernel_id={kernel_id}) is interrupted.")
+            mylogger.info(f"Kernel(id='{kernel_id}') is interrupted.")
 
     async def delete(self, k_id:Optional[str]=None, action:Optional[str]=None):
         """
@@ -148,17 +153,21 @@ class KernelHandler(ApplicationHandler):
                 self.set_status(404, reason=f"{self.request.uri} is not found.")
                 self.finish()
         except KeyError:
-            self.set_status(500, reason=f"kernel_id({k_id}) is not found in KM.")
+            self.set_status(404, reason=f"kernel_id({k_id}) is not found.")
             self.finish()
            
     async def kernel_shutdown(self, kernel_id:Optional[str]=None) -> None:
         """
         カーネルを停止する
+
+        存在しない`kernel_id`が渡された場合, `KeyError`を投げる  
+        `kernel_id=None`の場合, 指定されたカーネルのみを停止する
         """
         # shutdown all
         if kernel_id is None:
             await g.km.shutdown_all(now=True)
             self.finish({"DESCR": "All kernel is successfully shutted down."})
+            mylogger.info("Shutdown all kernels.")
         # shutdown specified kernel
         else:
             try:
@@ -168,6 +177,7 @@ class KernelHandler(ApplicationHandler):
             else:
                 self.finish({"kernel_id": kernel_id,
                              "DESCR": f"Kernel({kernel_id}) is successfully shutted down."})
+                mylogger.info(f"Shutdown a kernel(id='{kernel_id}')")
 
     def on_finish(self):
         g.km.updated.set()
